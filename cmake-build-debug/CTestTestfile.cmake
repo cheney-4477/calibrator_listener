a0e2685965a11fbcac648e8ab16101347b70e155
chenery <chenery@deepblueai.com>
2021-08-11 15:53:46 +0800

    delete cmake-build-debug

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/momo/workspace/rbt_ws/src/calibrator_listener
-# Build directory: /home/momo/workspace/rbt_ws/src/calibrator_listener/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
